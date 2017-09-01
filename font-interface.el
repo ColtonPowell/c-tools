914d4a5aeaf6a40aae4a634ea4de8870e4765e55
Colton Powell <Colton.Powell.1995@gmail.com>
2017-09-01 07:04:05 -0700

    added font increment/decrement settings

--- font-interface.el
+++ font-interface.el
@@ -4,6 +4,29 @@
 ;; Author: Colton Powell
 
 ;;; Code:
+;; The amount to increment/decrement font size by.
+(defvar font-margin 10)
+
+(defun font-size-increment ()
+  "An interactive function that allows you to increment your font size."
+  (interactive)
+  (let* ((current-font-size (face-attribute 'default :height))
+	 (new-font-size (+ current-font-size font-margin)))
+    (set-face-attribute 'default nil
+			:height new-font-size)
+    )
+  )
+
+(defun font-size-decrement ()
+  "An interactive function that allows you to decrement your font size."
+  (interactive)
+  (let* ((current-font-size (face-attribute 'default :height))
+	 (new-font-size (- current-font-size font-margin)))
+    (set-face-attribute 'default nil
+			:height new-font-size)
+    )
+  )
+
 
 (provide 'font-interface)
 ;;; font-interface ends here
