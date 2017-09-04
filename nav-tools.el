c58a70b8c31235ba0d7e2774eef4790783680e44
Colton Powell <Colton.Powell.1995@gmail.com>
2017-09-04 12:46:12 -0700

    Finished original purpose of nav-tools.el

--- nav-tools.el
+++ nav-tools.el
@@ -5,9 +5,161 @@
 
 ;;; Code:
 
-;; Return the number of lines on the page, taken right out of
-;; simple.el with a few mods. Just returns total instead of messaging
-;; everything.
+;; The actual functions called to move to the next/previous section(s):
+(defun next-section(&optional n)
+  "Move the cursor to the n'th next section. If n is nil, then
+move the cursor to the next section. 
+
+Let a section be defined as a line of text that is at the beginning or
+end of a block of text. It has at least one line of whitespace before
+or after it.
+
+This is a section
+This is NOT a section
+This is NOT a section
+This is NOT a section
+This is a section
+
+This is a section
+
+This is a section
+This is a section
+
+This tool is intended to make file traversal more quick by
+allowing you to skip through entire blocks of both text and
+whitespace with the press of a single key.
+"
+  (interactive)
+  (if (not n)
+      (setq n 1))
+  (while (> n 0)
+    (if (current-line-blank-p)
+	(next-section-from-ws)
+      (next-section-from-text))
+    (setq n (- n 1))))
+
+(defun previous-section(&optional n)
+  "Move the cursor to the n'th previous section. If n is nil, then
+move the cursor to the previous section. 
+
+Let a section be defined as a line of text that is at the beginning or
+end of a block of text. It has at least one line of whitespace before
+or after it.
+
+This is a section
+This is NOT a section
+This is NOT a section
+This is NOT a section
+This is a section
+
+This is a section
+
+This is a section
+This is a section
+
+This tool is intended to make file traversal more quick by
+allowing you to skip through entire blocks of both text and
+whitespace with the press of a single key.
+"
+  (interactive)
+  (if (not n)
+      (setq n 1))
+  (while (> n 0)
+    (if (current-line-blank-p)
+        (previous-section-from-ws)
+      (previous-section-from-text))
+    (setq n (- n 1))))
+
+;; get to the next section from text.
+(defun next-section-from-text()
+  "Move the cursor to the next section assuming it is placed in a
+  line of text."
+  ;; eval (get-num-page-lines) here to avoid repeated, unnecessary evals
+  (let ((lines-in-buffer (get-num-page-lines))
+	(at-eof '(> (line-number-at-pos) lines-in-buffer)))
+
+    ;; Cases (must always be (not at-eof)):
+    ;; - Next line is blank -> move to it and (next-section-from-ws)
+    (if (and (next-line-blank-p) (not (eval at-eof)))
+	(progn
+	  (forward-line)
+	  (next-section-from-ws))
+    ;; - Next line is not blank -> Move to last non-ws line that is
+    ;; not eof
+      (while (and (not (next-line-blank-p)) (not (eval at-eof)))
+	(forward-line)))
+
+    ;; Notify user if eof reached.
+    (if (eval at-eof)
+	(message "Reached end of file."))))
+
+;; get to the next section from whitespace.
+(defun next-section-from-ws()
+  "Move the cursor to the next section assuming it is placed in a
+  line of only whitespace."
+  (let ((lines-in-buffer (get-num-page-lines))
+	(at-eof '(> (line-number-at-pos) lines-in-buffer)))
+    
+    ;; move to next line while current line is blank and not at-eof
+    (while (and (current-line-blank-p) (not (eval at-eof)))
+      (forward-line))
+    
+    ;; Notify user when limit reached
+    (if (eval at-eof)
+	(message "Reached end of file."))))
+
+(defun previous-section-from-text()
+  "Move the cursor to the previous section assuming it is placed in a
+  line of text."
+  (let ((at-bof '(= (line-number-at-pos) 1)))
+    ;; Cases (must always be (not at-bof)):
+    ;; - Previous line is blank -> move to it and (previous-section-from-ws)
+    (if (and (previous-line-blank-p) (not (eval at-bof)))
+	(progn
+	  (forward-line -1)
+	  (previous-section-from-ws))
+    ;; - Previous line is not blank -> Move to last non-ws line that is
+    ;; not bof
+      (while (and (not (previous-line-blank-p)) (not (eval at-bof)))
+	(forward-line -1)))
+
+    ;; Notify user if bof reached.
+    (if (eval at-bof)
+	(message "Reached beginning of file."))))
+
+(defun previous-section-from-ws()
+  "Move the cursor to the previous section assuming it is placed in a
+  line of only whitespace."
+  (let ((at-bof '(= (line-number-at-pos) 1)))
+    ;; move to previous line while current line is blank and not at-eof
+    (while (and (current-line-blank-p) (not (eval at-bof)))
+      (forward-line -1))
+    ;; Notify user when bof reached
+    (if (eval at-bof)
+	(message "Reached beginning of file."))))
+
+(defun current-line-blank-p()
+  "Returns t if the current line is whitespace."
+    (let ((current-line (thing-at-point 'line)))
+      (if (eq (string-match-p "^[[:space:]]*$" current-line) 0)
+	  t)))
+
+(defun next-line-blank-p()
+  "Returns t if the next line is whitespace."
+  (save-excursion
+    (forward-line)
+    (let ((current-line (thing-at-point 'line)))
+      (if (eq (string-match-p "^[[:space:]]*$" current-line) 0)
+	  t))))
+
+(defun previous-line-blank-p()
+  "Returns t if the previous line is whitespace."
+  (save-excursion
+    (forward-line -1)
+    (let ((current-line (thing-at-point 'line)))
+      (if (eq (string-match-p "^[[:space:]]*$" current-line) 0)
+	  t))))
+
 (defun get-num-page-lines ()
   "Return the number of lines on current page. A modified version of
 count-lines-page."
@@ -23,58 +175,6 @@ count-lines-page."
       (setq beg (point))
       (setq total (count-lines beg end))
       total)))
-
-(defun skip-block())
-(defun skip-ws())
-(defun next-section())
-(defun previous-section()) 
-
-
-
-
-;; navigate to the beginning of the next block of text
-(defun next-block()
-  "Navigate to the beginning of the next block of text."
-  (interactive)
-  ;; Uses lines-in-buffer to avoid continually calling
-  ;; get-num-page-lines. Needs at-eof to check when eof reached.
-  (let* ((lines-in-buffer (get-num-page-lines))
-	(at-eof '(> (line-number-at-pos) lines-in-buffer)))
-    ;; While cur line not blank and not EOF
-    (while (and (not (current-line-blank-p)) (not (eval at-eof)))
-      (forward-line))
-    ;; While cur line blank and not EOF
-    (while (and (current-line-blank-p) (not (eval at-eof)))
-      (forward-line))
-    ;; Notify user of error at EOF
-    (if (eval at-eof)
-    (message "End of file reached."))))
-
-;; navigate to the end of the previous block of text
-(defun previous-block()
-  "Navigate to the end of the previous block of text."
-  (interactive)
-  ;; Need to check when beginning of file (bof) reached
-  (let* ((at-bof '(= (line-number-at-pos) 0)))
-    ;; Move through the current block
-    (while (and (not (current-line-blank-p)) (not (eval at-bof)))
-      (forward-line -1))
-    ;; While cur line blank and not BOF
-    (while (and (current-line-blank-p) (not (eval at-bof)))
-      (forward-line -1))
-    ;; Notify user of error at BOF
-    (if (eval at-bof)
-    (message "Beginning of file reached."))))
-
-;; determine if the current line is blank
-(defun is-current-line-blank-p()
-  (catch 'blank-line
-    ;; store the current line (under point) in current-line
-    (let ((current-line (thing-at-point 'line)))
-      ;; if the starting point of the line has all whitespace after it
-      (if (eq (string-match-p "^[[:space:]]*$" current-line) 0)
-	  ;; return t
-	  (throw 'blank-line t)))))
 
 (provide 'nav-tools)
 ;;; nav-tools.el ends here.
