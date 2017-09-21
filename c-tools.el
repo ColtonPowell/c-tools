00b92ada94caccc06c85134d11e288348a0a134d
Colton Powell <Colton.Powell.1995@gmail.com>
2017-09-21 15:54:18 -0700

    merging to master

--- c-tools.el
+++ c-tools.el
@@ -5,16 +5,6 @@
 
 ;;; Code:
 
-(defun make-makefile()
-  "Creates a very simple c/c++ makefile for when you're feeling
-  extremely, extremely lazy."
-  (interactive)
-  ;; Use (split-string (buffer-name) "\\.") to get a list ("c-tools"
-  ;; "el") etc. Traverse to last elem (file type), read it, and decide
-  ;; if c/c++.
-  (let* ((buf-name (buffer-name))))
-  )
-
 (require 'resize-window-mode)
 (require 'font-interface)
 (require 'nav-tools)
