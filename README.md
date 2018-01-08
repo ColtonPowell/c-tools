c61a4871e0660eecf327abc3605fc03bc52f7602
Colton Powell <Ctpowell@scu.edu>
2018-01-07 18:31:21 -0800

    Updated resize-window-mode to window-manage-mode, with much more functionality

--- README.md
+++ README.md
@@ -16,30 +16,28 @@ Useful info:
 ##### nav-tools
 A thing that helps you get around a little easier.
 
-- `next-block-soe(&optional n)` and `previous-block-soe(&optional n)` move to the start or end (soe; the first or last line) of the next/previous block of text.
+###### Info:
+- `next-block-soe(&optional n)` and `previous-block-soe(&optional n)` move to the start or end of the next/previous block of text.
 
 - (Also feel free to use `backward-delete-word` if you're tired of backward-kill-word cluttering your clipboard.)
 
-##### resize-window-mode
-###### Development MOVED to https://github.com/coltonpowell/window-manage-mode
-A minor mode for resizing the window.
+##### window-manage-mode
+A mode to encompass all of your emacs window management (moving, resizing, swapping, deleting, etc.) needs!
 
-  - Use `(rwm-toggle-default-keys)` ONCE to enable default keys (and again to disable them), which are shown below:
-    - `C-p` and `<up>` (up arrow) enlarge your current window vertically.
-    - `C-n` and `<down>` shrink your current window vertically.
-    - `C-f` and `<right>` enlarge your current window horizontally.
-    - `C-b` and `<left>` shrink your current window horizontally.
-    - `C-g` and `<escape>` exit resize-window-mode
+###### Info:
+- To enable extra functionality, install `ace-window` (version >= 0.9.0) and set `wmm-require-ace-window` to `t`.
 
-  - Use the `define-key` function to customize keybinds
-  - Change `rwm-margin` to change the number of resizes per keystroke. Default value is 3, recommended is 1-5.
+- Change `wmm-multiplier` to modify the power of `window-manage-mode`s resizing functions.
+
+- `(describe-function 'window-manage-mode)` after installation and setup to see keybinds.
 
 ##### font-interface
 A quicker way to test and change your font.
 
+###### Info:
 - `increment-font-size` and `decrement-font-size` inc/decrement the height of the font by `font-margin`, which is equal to 10 by default.
 
-- You can change `font-margin` and the face you're editing (`face-to-edit`) by quickly using the `set-font-margin` and `set-face-to-edit` functions (respectively), or set them in your .emacs file 
+- You can change `font-margin` and the face you're editing (`face-to-edit`) by quickly using the `set-font-margin` and `set-face-to-edit` functions (respectively), or set them in your .emacs file
 
 - Change the font family quickly by using `set-font-family`
     
---
+++ window-manage-mode.el
@@ -0,0 +1,155 @@
+;;; window-manage-mode.el --- Manage and navigate through windows easily
+;; Copyright (C) 2017 Colton Powell
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; Author: Colton Powell
+;; Package-Requires: ((ace-window "0.9.0"))
+
+;;; Code:
+
+;; Determines the number of resize actions per keypress.
+(defcustom wmm-multiplier 3
+  "Multiplies the number of actions for the `window-manage-mode' resize
+functions. Default value is 3. Recommended values are 1-5."
+  :type 'integer)
+
+(defcustom wmm-require-ace-window nil
+  "Specifies whether `ace-window' keybinds are enabled in
+`window-manage-mode'. If this is the case, the value should be
+  t. Default value is nil."
+  :type 'boolean)
+
+;; Set the default multiplier from the minibuffer
+(defun wmm-set-multiplier (new-multiplier)
+  "Set a new multiplier for window-manage-mode."
+  (interactive (list (read-number "Enter a new multiplier: " wmm-multiplier)))
+  (setq wmm-multiplier new-multiplier)
+  (message "%d" wmm-multiplier))
+
+;; Functions for resizing the windows interactively
+(defun wmm-enlarge-window-vertically()
+  "Enlarge the window vertically `wmm-multiplier' times."
+  (interactive)
+  (message "Enlarging window vertically %d times." wmm-multiplier)
+  (enlarge-window wmm-multiplier))
+
+(defun wmm-shrink-window-vertically()
+  "Shrink the window vertically `wmm-multiplier' times."
+  (interactive)
+  (message "Shrinking window vertically %d times." wmm-multiplier)
+  (shrink-window wmm-multiplier))
+
+(defun wmm-shrink-window-horizontally()
+  "Shrink the window horizontally `wmm-multiplier' times."
+  (interactive)
+  (message "Shrinking window horizontally %d times." wmm-multiplier)
+  (shrink-window-horizontally wmm-multiplier))
+
+(defun wmm-enlarge-window-horizontally()
+  "Enlarge the window vertically `wmm-multiplier' times."
+  (interactive)
+  (message "Enlarging window horizontally %d times." wmm-multiplier)
+  (enlarge-window-horizontally wmm-multiplier))
+
+;; Determines whether default keys are toggled on/off
+(defcustom wmm-default-keys-on t
+  "This variable tracks the state of the default keybindings, and
+is used in `wmm-toggle-default-keys'. DO NOT modify the value of
+this variable."
+  :type 'boolean)
+
+;; window-manage-mode's keymap. A blank map by default, customizable
+;; by the user or by (wmm-toggle-default-keys)
+(defcustom window-manage-mode-map
+  (if wmm-default-keys-on
+  (let ((map (make-sparse-keymap)))
+    ;; ========== Enable Window Sizing Keys ==========
+    ;; Enable default vertical enlarge keys
+    (define-key map (kbd "<M-up>") 'wmm-enlarge-window-vertically)
+    (define-key map (kbd "M-p") 'wmm-enlarge-window-vertically)
+
+    ;; Enable default vertical shrink keys
+    (define-key map (kbd "<M-down>") 'wmm-shrink-window-vertically)
+    (define-key map (kbd "M-n") 'wmm-shrink-window-vertically)
+
+    ;; Enable default horizontal enlarge keys
+    (define-key map (kbd "<M-right>") 'wmm-enlarge-window-horizontally)
+    (define-key map (kbd "M-f") 'wmm-enlarge-window-horizontally)
+
+    ;; Enable default horizontal shrink keys
+    (define-key map (kbd "<M-left>") 'wmm-shrink-window-horizontally)
+    (define-key map (kbd "M-b") 'wmm-shrink-window-horizontally)
+
+    ;; ========== Enable Window Switching Keys ==========
+    ;; Enable default up window switch keys
+    (define-key map (kbd "<C-up>") 'windmove-up)
+    (define-key map (kbd "C-p") 'windmove-up)
+    
+    ;; Enable default down window switch keys
+    (define-key map (kbd "<C-down>") 'windmove-down)
+    (define-key map (kbd "C-n") 'windmove-down)
+    
+    ;; Enable default left window switch keys
+    (define-key map (kbd "<C-right>") 'windmove-right)
+    (define-key map (kbd "C-f") 'windmove-right)
+    
+    ;; Enable default right window switch keys
+    (define-key map (kbd "<C-left>") 'windmove-left)
+    (define-key map (kbd "C-b") 'windmove-left)
+
+    ;; Enable easier window swap key
+    (define-key map (kbd "o") 'other-window)
+
+    ;; Delete current window
+    (define-key map (kbd "0") 'delete-window)
+ 
+    ;; ========== Enable ace-window keys if enabled ==========
+    (when wmm-require-ace-window
+      (require 'ace-window)
+      (define-key map (kbd "a") 'ace-window)
+      (define-key map (kbd "s") 'ace-select-window)
+      (define-key map (kbd "d") 'ace-delete-window)
+      (define-key map (kbd "f") 'ace-swap-window))
+
+    ;; ========== Enable Exit Keys ==========
+    (define-key map (kbd "C-g") 'window-manage-mode)
+    (define-key map (kbd "<escape>") 'window-manage-mode)
+    
+    map)
+  (make-sparse-keymap))
+  "The `window-manage-mode' keymap. Set wmm-require-ace-window to t to
+  enable ace-window keybinds. Use (describe-function
+  'window-manage-mode) to see keybinds.")
+
+(defun window-manage-mode-on()
+  (unless (minibufferp)
+    (window-manage-mode))
+  "Function used by `global-window-manage-mode' to activate
+  `window-manage-mode' in every buffer.")
+
+(define-minor-mode window-manage-mode
+  nil
+  nil
+  window-manage-mode-map
+  "DO NOT USE! Use `global-window-manage-mode' instead")
+
+;; Could use autoload magic comment here?
+(define-global-minor-mode global-window-manage-mode
+  window-manage-mode
+  window-manage-mode-on
+  "Activates `window-manage-mode' in all buffers.")
+
+(provide 'window-manage-mode)
+;;; window-manage-mode ends here
